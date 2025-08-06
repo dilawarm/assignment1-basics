551a41b38d104910807193b3345fb102031b0e05
Dilawar Mahmood <dilawarmm@outlook.com>
2025-08-05 22:40:23 -0400

    fix import

--- cs336_basics/data/__init__.py
+++ cs336_basics/data/__init__.py
@@ -1,13 +1,9 @@
 """Data module for OpenWebText dataset handling."""
 
 from .dataloader import OpenWebTextDataModule, create_dataloaders
-from .local_dataloader import LocalOpenWebTextDataModule, LocalOpenWebTextDataset, create_local_dataloaders
 
 __all__ = [
     "OpenWebTextDataset",
     "OpenWebTextDataModule",
     "create_dataloaders",
-    "LocalOpenWebTextDataset",
-    "LocalOpenWebTextDataModule",
-    "create_local_dataloaders",
 ]
