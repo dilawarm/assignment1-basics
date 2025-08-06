b593b4bf8fb22214ee3a309eeaff2ec050573a44
Dilawar Mahmood <dilawarmm@outlook.com>
2025-08-05 22:38:35 -0400

    fix import

--- cs336_basics/training/__init__.py
+++ cs336_basics/training/__init__.py
@@ -1,5 +1,5 @@
 """Training module for H100-optimized transformer."""
 
-from .trainer import Trainer, TrainingConfig
+from .trainer import Trainer, TrainingConfig, H100OptimizedTrainer
 
-__all__ = ["Trainer", "TrainingConfig"]
+__all__ = ["Trainer", "TrainingConfig", "H100OptimizedTrainer"]
